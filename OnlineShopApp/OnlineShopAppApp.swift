27fd181523b7a69def97e87905b119b435ce74b8
Boris Kravchenko <allmycalifornia@gmail.com>
2024-12-18 09:40:00 +0300

    added push-notifications
    with Firebase

--- OnlineShopApp/OnlineShopAppApp.swift
+++ OnlineShopApp/OnlineShopAppApp.swift
@@ -1,15 +1,57 @@
 
 import SwiftUI
-import FirebaseCore
+import Firebase
+import FirebaseMessaging
+import UserNotifications
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        
+        // Запрос разрешения на отправку уведомлений
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if let error = error {
+                print("Error requesting notifications authorization: \(error)")
+            } else {
+                print("Notifications permission granted: \(granted)")
+            }
+        }
+        
+        // Регистрация для удалённых уведомлений
+        DispatchQueue.main.async {
+            UIApplication.shared.registerForRemoteNotifications()
+        }
+        
+        Messaging.messaging().delegate = self
+        return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // Передаём APNs токен в Firebase
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("FCM Token: \(fcmToken ?? "No Token")")
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                 willPresent notification: UNNotification,
+                                 withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+
+        center.setBadgeCount(0) { error in
+                    if let error = error {
+                        print("Failed to reset badge count: \(error)")
+                    } else {
+                        print("Badge count reset successfully")
+                    }
+                }
+        completionHandler([.banner, .sound, .badge])
+    }
 }
-
 
 @main
 struct OnlineShopAppApp: App {
